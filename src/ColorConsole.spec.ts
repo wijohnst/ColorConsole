fa076c686be342b1cff61d125158fc9042a65cab
wijohnst <will_johnston@goodyear.com>
2023-01-10 09:25:00 -0500

    Adds tests

--- src/ColorConsole.spec.ts
+++ src/ColorConsole.spec.ts
@@ -1,7 +1,8 @@
 import { generateStringWithColorEscapeCode, colorMap } from './ColorConsole';
 
 describe('ColorConsole unit tests', () => {
-	it('Should return the correct string when `red_` is passed', () => {
-		expect(generateStringWithColorEscapeCode("red_TEST")).toEqual(`${colorMap.red_}TEST`);
-	})
+	const colorMapValues = Object.entries(colorMap);
+	test.each(colorMapValues)('when passing %s, expect the correct escape code',(humanReadableColorString, escapeCode) => {
+		expect(generateStringWithColorEscapeCode(`${humanReadableColorString}Test`)).toEqual(`${escapeCode}Test`);
+	})	
 })
